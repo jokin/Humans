6d14033fd8eefcdc8becb716943330fa1cf49f6b
Peter Drier <peter.drier@gmail.com>
2026-03-27 14:40:39 +0100

    fix: exclude former volunteers from ticket report

--- src/Humans.Web/Controllers/TicketController.cs
+++ src/Humans.Web/Controllers/TicketController.cs
@@ -393,7 +393,7 @@ public class TicketController : HumansControllerBase
 
         var activeHumans = users
             .Where(u => u.Profile is not null &&
-                u.TeamMemberships.Any(tm => tm.TeamId == volunteersTeamId))
+                u.TeamMemberships.Any(tm => tm.TeamId == volunteersTeamId && tm.LeftAt == null))
             .ToList();
 
         var filteredHumans = FilterWhoHasntBoughtHumans(
